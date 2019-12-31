bee44731bd08aefc83deca5c57e65e3574ba77ca
张磊 <zhang.lei@xyb2b.com>
2019-12-31 17:02:12 +0800

    修改代码

--- 使用WSGI接口处理web/hello.py
+++ 使用WSGI接口处理web/hello.py
@@ -1,3 +1,3 @@
-def application(start_response,environ):
-	start_response('200 OK',[('Content-Type','text/html')])
-	return [b'<h1>Hello, web!</h1>']
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [b'<h1>Hello, web!</h1>']
--- 使用WSGI接口处理web/server.py
+++ 使用WSGI接口处理web/server.py
@@ -1,12 +1,11 @@
-#负责启动WSGI服务器，加载application()函数
-#Python内置了一个WSGI服务器，这个模块叫wsgiref
-
+# server.py
+# 从wsgiref模块导入:
 from wsgiref.simple_server import make_server
 # 导入我们自己编写的application函数:
 from hello import application
 
 # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
-httpd = make_server('',8000,application)
+httpd = make_server('', 8000, application)
 print('Serving HTTP on port 8000...')
 # 开始监听HTTP请求:
 httpd.serve_forever()
