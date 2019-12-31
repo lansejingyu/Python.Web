dfcabe09689a6a1952323ea181b6f89ccc9fc281
张磊 <zhang.lei@xyb2b.com>
2019-12-31 15:40:42 +0800

    增加注释

--- 使用WSGI接口处理web/server.py
+++ 使用WSGI接口处理web/server.py
@@ -10,3 +10,6 @@ httpd = make_server('',8000,application)
 print('Serving HTTP on port 8000...')
 # 开始监听HTTP请求:
 httpd.serve_forever()
+
+#无论多么复杂的Web应用程序，入口都是一个WSGI处理函数。
+# HTTP请求的所有输入信息都可以通过environ获得，HTTP响应的输出都可以通过start_response()加上函数返回值作为Body。
